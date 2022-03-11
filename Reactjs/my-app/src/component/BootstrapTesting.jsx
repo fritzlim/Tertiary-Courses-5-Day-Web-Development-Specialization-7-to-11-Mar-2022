7e43fe8a237d0bcfa16f249cc3a2774cf913ed31
Fritz Lim <fritzlim@gmail.com>
2022-03-11 18:32:21 +0800

    Able to show an Alert and Toast
    Adapted from https://react-bootstrap.github.io/components/alerts/ and https://github.com/react-bootstrap/code-sandbox-examples/blob/master/basic/src/App.js (taken from https://github.com/react-bootstrap/code-sandbox-examples)

--- Reactjs/my-app/src/component/BootstrapTesting.jsx
+++ Reactjs/my-app/src/component/BootstrapTesting.jsx
@@ -1,10 +1,19 @@
 import Button from "react-bootstrap/Button";
-// import Alert from "react-bootstrap/alert";
+import Alert from "react-bootstrap/Alert";
+import Toast from "react-bootstrap/Toast";
 
 function BootstrapTesting(props) {
   return (
-    <Button variant="primary">Primary</Button>
-    // <Alert variant="primary">Hello</Alert>
+    <>
+      <Button variant="primary">Primary</Button>
+      <Alert variant="primary">Hello</Alert>
+      <Toast show={true}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>This is a toast</Toast.Body>
+      </Toast>
+    </>
   );
 }
 
