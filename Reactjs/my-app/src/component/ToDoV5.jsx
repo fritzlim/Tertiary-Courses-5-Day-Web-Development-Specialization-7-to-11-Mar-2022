4f635dc38aed11f6324171439d20ed21c362da93
Fritz Lim <fritzlim@gmail.com>
2022-03-11 14:08:42 +0800

    implemented conditional rendering and delete button in ToDoV5

--- Reactjs/my-app/src/component/ToDoV5.jsx
+++ Reactjs/my-app/src/component/ToDoV5.jsx
@@ -3,46 +3,79 @@ import React from "react";
 class ToDoV5 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { item1: "Groceries", item2: "Lunch" };
+    this.state = {
+      item1: "Groceries",
+      item2: "Lunch",
+      isItem1Deleted: false,
+      isItem2Deleted: false,
+    };
   }
 
   setItem(e) {
-    if (e.target.value == "item1") {
+    if (e.target.value === "item1") {
       this.setState({ item1: "Completed" });
-    } else if (e.target.value == "item2") {
+    } else if (e.target.value === "item2") {
       this.setState({ item2: "Completed" });
     }
     // console.log(e.target.value);
   }
 
+  deleteItem = (e) => {
+    if (e.target.value === "item1" && this.state.item1 === "Completed") {
+      this.setState({ isItem1Deleted: true });
+    } else if (e.target.value === "item2" && this.state.item2 === "Completed") {
+      this.setState({ isItem2Deleted: "Completed" });
+    }
+  };
+
   render() {
-    const { item1, item2 } = this.state;
+    const { item1, item2, isItem1Deleted, isItem2Deleted } = this.state;
     return (
       <>
         <h2>ToDo Version 5 (A ToDo list with buttons)</h2>
         <ol>
-          <li>
-            {item1}
-            <button
-              value="item1"
-              onClick={(e) => {
-                this.setItem(e);
-              }}
-            >
-              Done
-            </button>
-          </li>
-          <li>
-            {item2}
-            <button
-              value="item2"
-              onClick={(e) => {
-                this.setItem(e);
-              }}
-            >
-              Done
-            </button>
-          </li>
+          {!isItem1Deleted && (
+            <li>
+              {item1}
+              <button
+                value="item1"
+                onClick={(e) => {
+                  this.setItem(e);
+                }}
+              >
+                Done
+              </button>
+              <button
+                value="item1"
+                onClick={(e) => {
+                  this.deleteItem(e);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          )}
+          {!isItem2Deleted && (
+            <li>
+              {item2}
+              <button
+                value="item2"
+                onClick={(e) => {
+                  this.setItem(e);
+                }}
+              >
+                Done
+              </button>
+              <button
+                value="item2"
+                onClick={(e) => {
+                  this.deleteItem(e);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          )}
         </ol>
       </>
     );
